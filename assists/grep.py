9398385d3ac06419b25d34de21501bc7ac0e8ac3
Bruce Ashfield <bruce.ashfield@xilinx.com>
2020-05-19 16:41:13 -0400

    lopper/assists: add sample/siple command line grep assist
    Signed-off-by: Bruce Ashfield <bruce.ashfield@xilinx.com>

---
+++ assists/grep.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+#/*
+# * Copyright (c) 2020 Xilinx Inc. All rights reserved.
+# *
+# * Author:
+# *       Bruce Ashfield <bruce.ashfield@xilinx.com>
+# *
+# * SPDX-License-Identifier: BSD-3-Clause
+# */
+
+import struct
+import sys
+import types
+import unittest
+import os
+import getopt
+import re
+import subprocess
+import shutil
+from pathlib import Path
+from pathlib import PurePath
+from io import StringIO
+import contextlib
+import importlib
+from lopper import Lopper
+from lopper import LopperFmt
+from lopper_tree import LopperAction
+import lopper
+
+def is_compat( node, compat_string_to_test ):
+    if re.search( "module,grep", compat_string_to_test):
+        return grep
+    return ""
+
+# tests for a bit that is set, going fro 31 -> 0 from MSB to LSB
+def check_bit_set(n, k):
+    if n & (1 << (k)):
+        return True
+
+    return False
+
+# tgt_node: is the openamp domain node number
+# sdt: is the system device tree
+def grep( tgt_node, sdt, options ):
+    try:
+        verbose = options['verbose']
+    except:
+        verbose = 0
+
+    try:
+        args = options['args']
+    except:
+        args = []
+
+    if verbose:
+        print( "[INFO]: cb: grep( %s, %s, %s, %s )" % (tgt_node, sdt, verbose, args))
+
+    node_regex = ""
+    tgt_regex = args[0]
+    if len(args) == 2:
+        node_regex = args[1]
+
+    # for n in sdt.tree:
+    #     print( "n: %s" % n )
+
+    nodes = []
+    try:
+        nodes = sdt.tree.nodes(node_regex)
+    except:
+        print( "[ERROR]: grep: nodes %s not found" % node_regex )
+        sys.exit(1)
+
+
+    matches = {}
+    for n in nodes:
+        try:
+            #print( "looking for: %s in %s" % (tgt_regex,n) )
+            match = n[tgt_regex]
+            #print( "match: %s" % match )
+            matches[n.abs_path] = match
+            #print( "matches is now: %s" % matches )
+        except:
+            pass
+
+    if matches:
+        for m in matches.keys():
+            print( "%s: %s" % (m,matches[m]))
+    else:
+        print( "%s: not found" % tgt_regex )
+
+    return True
