2766ec78a5b3b2088312ea89879b12aaab97e39d
Ben Levinsky <ben.levinsky@xilinx.com>
2020-06-25 09:13:09 -0700

    assists: openamp-xlnx.py: update so that both header file and device tree are output correctly
    previously, both device tree and header output would only work for 1 openamp case. Now update assist
    to accomodate 1 universal input device tree and change based on lop file but still output correct
    device tree for linux as well as header for openamp applications
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>

--- assists/openamp-xlnx.py
+++ assists/openamp-xlnx.py
@@ -7,6 +7,7 @@
 # * SPDX-License-Identifier: BSD-3-Clause
 # */
 
+import copy
 import struct
 import sys
 import types
@@ -39,56 +40,68 @@ def write_one_carveout(f, prefix, addr_prop, range_prop):
     f.write("#define ")
     f.write(prefix+"RANGE\t"+range_prop+"U\n")
 
-def write_openamp_virtio_rpmsg_info(f, carveout_list, options):
+def write_openamp_virtio_rpmsg_info(f, carveout_list, options, is_kernel_case):
     symbol_name = "CHANNEL_0_MEM_"
     current_channel_number = 0
     current_channel_count = 0 # if == 4 then got complete channel range
     vring_mems = []
-
+    rsc_mem_pa = -1
+    shared_mem_size = -1
     for i in carveout_list:
-        if "channel" in i[0]:
+        if "channel" in i[0] or "rpu" in i[0]:
             # save channel number
-            channel_number = int((re.search("channel([0-9]+)", i[0]).group(1) ))
+            if "channel" in i[0]:
+                channel_number = int((re.search("channel([0-9]+)", i[0]).group(1) ))
+            if "rpu" in i[0]:
+                channel_number = int((re.search("rpu([0-9]+)", i[0]).group(1) ))
             if channel_number != current_channel_number:
                 symbol_name = "CHANNEL_"+str(channel_number)+"_MEM_"
                 current_channel_number = channel_number
 
-            if "vdev0buffer" in i[0]:
-                current_channel_count += 1
-                f.write("#define "+symbol_name+"SHARED_MEM_SIZE\t"+i[1][1]+"\n")
-                f.write("#define "+symbol_name+"SHARED_BUF_PA\t"+i[1][0]+"\n")
-            elif "vdev0vring0" in i[0]:
-                current_channel_count += 1
-                f.write("#define "+symbol_name+"SHARED_MEM_PA\t"+i[1][0]+"\n")
-                f.write("#define "+symbol_name+"RING_TX\t"+i[1][0]+"\n")
-                f.write("#define "+symbol_name+"VRING_MEM_PA\t"+i[1][0]+"\n")
-                vring_mems.append(i[1][1])
-            elif "vdev0vring1" in i[0]:
-                vring_mems.append(i[1][1])
-                current_channel_count += 1
-                f.write("#define "+symbol_name+"RING_RX\t"+i[1][0]+"\n")
-            elif "elfload" in i[0]:
-                f.write("#define "+symbol_name+"RSC_MEM_PA\t"+hex( int( i[1][0],16)+0x20000 )+"\n")
-                f.write("#define "+symbol_name+"SHM_DEV_NAME\t\""+hex( int( i[1][0],16)+0x20000 ).replace("0x","")+".shm\"\n")
-                f.write("#define "+symbol_name+"SHARED_BUF_SIZE\t"+i[1][1]+"\n")
-                current_channel_count += 1
+        if "vdev0buffer" in i[0]:
+            current_channel_count += 1
+            f.write("#define "+symbol_name+"SHARED_MEM_SIZE\t"+i[1][3]+"\n")
+            shared_mem_size = int(i[1][3],16)
+            f.write("#define "+symbol_name+"SHARED_BUF_PA\t"+i[1][1]+"\n")
+        elif "vdev0vring0" in i[0]:
+            current_channel_count += 1
+            f.write("#define "+symbol_name+"SHARED_MEM_PA\t"+i[1][1]+"\n")
+            if is_kernel_case:
+                f.write("#define "+symbol_name+"RING_TX\tFW_RSC_U32_ADDR_ANY\n")
+            else:
+                f.write("#define "+symbol_name+"RING_TX\t"+i[1][1]+"\n")
+            f.write("#define "+symbol_name+"VRING_MEM_PA\t"+i[1][1]+"\n")
+            vring_mems.append(i[1][3])
+        elif "vdev0vring1" in i[0]:
+            vring_mems.append(i[1][3])
+            current_channel_count += 1
+            if is_kernel_case:
+                f.write("#define "+symbol_name+"RING_RX\tFW_RSC_U32_ADDR_ANY\n")
+            else:
+                f.write("#define "+symbol_name+"RING_RX\t"+i[1][1]+"\n")
+        elif "elfload" in i[0]:
+            f.write("#define "+symbol_name+"RSC_MEM_PA\t"+hex( int( i[1][1],16)+0x20000 )+"\n")
+            rsc_mem_pa =  int( i[1][1],16)+0x20000
+            f.write("#define "+symbol_name+"SHM_DEV_NAME\t\""+hex( int( i[1][1],16)+0x20000 ).replace("0x","")+".shm\"\n")
+            f.write("#define "+symbol_name+"SHARED_BUF_SIZE\t"+i[1][3]+"\n")
+            current_channel_count += 1
 
-            if current_channel_count == 4:
-                current_channel_count = 0
-                vring_mems_size_total = 0
-                for i in vring_mems:
-                    vring_mems_size_total += int(i,16)
-                f.write("#define "+symbol_name+"SHARED_BUF_OFFSET\t"+hex(vring_mems_size_total)+"\n")
-                f.write("#define "+symbol_name+"VRING_MEM_SIZE\t"+hex(vring_mems_size_total)+"\n")
-                vring_mem_size = 0
-                f.write("#define "+symbol_name+"RSC_MEM_SIZE\t0x2000UL\n")
-                f.write("#define "+symbol_name+"NUM_VRINGS\t2\n")
-                f.write("#define "+symbol_name+"VRING_ALIGN\t0x1000\n")
-                f.write("#define "+symbol_name+"VRING_SIZE\t256\n")
-                f.write("#define "+symbol_name+"NUM_TABLE_ENTRIES\t1\n")
-                f.write("#define MASTER_BUS_NAME\t\"platform\"\n")
-                f.write("#define REMOTE_BUS_NAME\t\"generic\"\n")
-
+        if current_channel_count == 4:
+            current_channel_count = 0
+            vring_mems_size_total = 0
+            for i in vring_mems:
+                vring_mems_size_total += int(i,16)
+            f.write("#define "+symbol_name+"SHARED_BUF_OFFSET\t"+hex(vring_mems_size_total)+"\n")
+            f.write("#define "+symbol_name+"VRING_MEM_SIZE\t"+hex(vring_mems_size_total)+"\n")
+            vring_mem_size = 0
+            f.write("#define "+symbol_name+"RSC_MEM_SIZE\t0x2000UL\n")
+            f.write("#define "+symbol_name+"NUM_VRINGS\t2\n")
+            f.write("#define "+symbol_name+"VRING_ALIGN\t0x1000\n")
+            f.write("#define "+symbol_name+"VRING_SIZE\t256\n")
+            f.write("#define "+symbol_name+"NUM_TABLE_ENTRIES\t1\n")
+            f.write("#define MASTER_BUS_NAME\t\"platform\"\n")
+            f.write("#define REMOTE_BUS_NAME\t\"generic\"\n")
+    return [rsc_mem_pa, shared_mem_size]
 
 def write_mem_carveouts(f, carveout_list, options):
     symbol_name = "CHANNEL_0_MEM_"
@@ -105,20 +118,20 @@ def write_mem_carveouts(f, carveout_list, options):
                 current_channel_number = channel_number
 
             if "vdev0buffer" in i[0]:
-                write_one_carveout(f, symbol_name+"VDEV0BUFFER_", i[1][0], i[1][1])
-                channel_range += int(i[1][1],16)
+                write_one_carveout(f, symbol_name+"VDEV0BUFFER_", i[1][1], i[1][3])
+                channel_range += int(i[1][3],16)
                 current_channel_count += 1
             elif "vdev0vring0" in i[0]:
-                write_one_carveout(f, symbol_name+"VDEV0VRING0_", i[1][0], i[1][1])
-                channel_range += int(i[1][1],16)
+                write_one_carveout(f, symbol_name+"VDEV0VRING0_", i[1][1], i[1][3])
+                channel_range += int(i[1][3],16)
                 current_channel_count += 1
             elif "vdev0vring1" in i[0]:
-                write_one_carveout(f, symbol_name+"VDEV0VRING1_", i[1][0], i[1][1])
-                channel_range += int(i[1][1],16)
+                write_one_carveout(f, symbol_name+"VDEV0VRING1_", i[1][1], i[1][3])
+                channel_range += int(i[1][3],16)
                 current_channel_count += 1
             elif "elfload" in i[0]:
-                write_one_carveout(f, symbol_name+"ELFLOAD_", i[1][0], i[1][1])
-                channel_range += int(i[1][1],16)
+                write_one_carveout(f, symbol_name+"ELFLOAD_", i[1][1], i[1][3])
+                channel_range += int(i[1][3],16)
                 current_channel_count += 1
 
             if current_channel_count == 4:
@@ -132,8 +145,7 @@ versal_ipi_lookup_table = { "0xff340000" : [63, 0x0000020 ] , "0xff360000" : [0 
 zynqmp_ipi_lookup_table = { "0xff310000" : [65, 0x1000000 ] , "0xff340000" : [0 , 0x100 ] }
 
 # given interrupt list, write interrupt base addresses and adequate register width to header file
-# TODO append memory carveout-related info
-def generate_openamp_file(ipi_list, carveout_list, options, platform):
+def generate_openamp_file(ipi_list, carveout_list, options, platform, is_kernel_case):
     if (len(options["args"])) > 0:
         f_name = options["args"][0]
     else:
@@ -159,7 +171,7 @@ def generate_openamp_file(ipi_list, carveout_list, options, platform):
         else:
             ipi += "_REMOTE_"
         f.write(ipi+"IPI_BASE_ADDR\t"+value+"U\n")
-        f.write("#define "+ipi+"IPI_NAME\t\""+value.replace("0x","")+".ipi\"\n")
+        f.write("#define "+ipi+"IPI_NAME\t\""+value.replace("0x","")+".ps_ipi\"\n")
 
         try:
             ipi_details_list = None
@@ -183,11 +195,12 @@ def generate_openamp_file(ipi_list, carveout_list, options, platform):
 
     f.write("\n")
     write_mem_carveouts(f, carveout_list, options)
-    write_openamp_virtio_rpmsg_info(f, carveout_list, options)
+    [ rsc_mem_pa, shared_mem_size ] = write_openamp_virtio_rpmsg_info(f, carveout_list, options, is_kernel_case)
     f.write("\n\n#endif /* OPENAMP_LOPPER_INFO_H_ */\n")
     f.close()
+    return [rsc_mem_pa,shared_mem_size]
 
-def parse_ipis_for_rpu(sdt, domain_node, rpu_cpu_node, options):
+def parse_ipis_for_rpu(sdt, domain_node, options):
     try:
         verbose = options['verbose']
     except:
@@ -196,17 +209,14 @@ def parse_ipis_for_rpu(sdt, domain_node, rpu_cpu_node, options):
     ipi_list = []
     for node in sdt.tree:
         if "ps_ipi" in node.abs_path:
-            ipi_list.append(node["reg"].hex()[0])
+            ipi_list.append(node["reg"].hex()[1])
 
     if verbose:
         print( "[INFO]: Dedicated IPIs for OpenAMP: %s" % ipi_list)
 
-    ipis_prop = LopperProp(name="ipis", value=ipi_list)
-    rpu_cpu_node + ipis_prop
     return ipi_list
 
-
-def parse_memory_carevouts_for_rpu(sdt, domain_node, rpu_cpu_node, memory_node, options):
+def parse_memory_carevouts_for_rpu(sdt, domain_node, memory_node, options):
     try:
         verbose = options['verbose']
     except:
@@ -215,11 +225,10 @@ def parse_memory_carevouts_for_rpu(sdt, domain_node, rpu_cpu_node, memory_node, 
     carveout_list = [] # string representation of mem carveout nodes
     dt_carveout_list = [] # values used for later being put into output DT's
     for node in sdt.tree:
-        if "channel" in node.abs_path and "openamp,xlnx,mem-carveout" in node['compatible'].value[0]:
+        if node.props("compatible") != [] and "openamp,xlnx,mem-carveout" in node['compatible'].value[0]:
             carveout_list.append( ( (str(node), str(node['reg']).replace("reg = <","").replace(">;","").split(" ")) ))
             for i in  node['reg'].int():
                 dt_carveout_list.append(i)
-
     prop = LopperProp("memory-region")
     prop.value = dt_carveout_list
     rpu_path = memory_node.abs_path
@@ -236,6 +245,8 @@ def parse_memory_carevouts_for_rpu(sdt, domain_node, rpu_cpu_node, memory_node, 
 
     return carveout_list
 
+
+
 def is_compat( node, compat_string_to_test ):
     if re.search( "openamp,xlnx-rpu", compat_string_to_test):
         return xlnx_openamp_rpu
@@ -248,31 +259,27 @@ def check_bit_set(n, k):
 
     return False
 
-# tgt_node: is the openamp domain node number
-# sdt: is the system device tree
-# TODO: this routine needs to be factored and made smaller
-def xlnx_openamp_rpu( tgt_node, sdt, options ):
+zynqmp_userspace_ipi_prop_table =  { "0xff340000" : [ "0 29 4" ] }
+def handle_rpmsg_userspace_case(tgt_node, sdt, options, domain_node, memory_node, rpu_node, rsc_mem_pa, shared_mem_size):
+    gic_node = sdt.tree["/amba-apu@0/interrupt-controller@f9010000"]
+    openamp_shm_node = sdt.tree["/amba/shm"]
+    openamp_shm_node["reg"].value = [0x0 , rsc_mem_pa, 0x0, shared_mem_size]
+    openamp_shm_node.sync ( sdt.FDT )
+    for node in sdt.tree:
+        if "ps_ipi" in node.abs_path:
+            prop = LopperProp("interrupt-parent")
+            prop.value = gic_node.phandle
+            node + prop
+            node.sync ( sdt.FDT )
+
+
+
+
+def handle_rpmsg_kernelspace_case(tgt_node, sdt, options, domain_node, memory_node, rpu_node):
     try:
         verbose = options['verbose']
     except:
         verbose = 0
-
-    if verbose:
-        print( "[INFO]: cb: xlnx_openamp_rpu( %s, %s, %s )" % (tgt_node, sdt, verbose))
-
-    domain_node = sdt.tree[tgt_node]
-
-    root_node = sdt.tree["/"]
-    platform = SOC_TYPE.UNINITIALIZED
-    if 'versal' in str(root_node['compatible']):
-        platform = SOC_TYPE.VERSAL
-    elif 'zynqmp' in str(root_node['compatible']):
-        platform = SOC_TYPE.ZYNQMP
-    else:
-        print("invalid input system DT")
-        return False
-
-
 
     try:
         cpu_prop_values = domain_node['cpus'].value
@@ -286,11 +293,8 @@ def xlnx_openamp_rpu( tgt_node, sdt, options ):
     if verbose:
         print( "[INFO]: cb cpu mask: %s" % cpu_mask )
 
-    # find the added rpu node
-    try:
-        rpu_node = sdt.tree[".*zynqmp-rpu" ]
-    except:
-        print( "[ERROR]: cannot find the target rpu node" )
+    if rpu_node == None:
+        print( "not valid input systemDT for openamp rpmsg kernelspace case")
         return False
 
     rpu_path = rpu_node.abs_path
@@ -303,7 +307,15 @@ def xlnx_openamp_rpu( tgt_node, sdt, options ):
         rpu_cpu_node = sdt.tree[ rpu_path + "/__cpu__" ]
     except:
         print( "[ERROR]: cannot find the target rpu node" )
-        return False
+        return  memory_node
+
+    # update mboxes value with phandles
+    for node in sdt.tree:
+        if "zynqmp_ipi" in node.abs_path and "mailbox" in node.abs_path:
+            if node.props('xlnx,open-amp,mailbox') != []:
+                zynqmp_ipi_mbox_phandle_value = node.phandle
+                rpu_cpu_node["mboxes"].value = [ zynqmp_ipi_mbox_phandle_value , 0x0, zynqmp_ipi_mbox_phandle_value, 0x1 ]
+                rpu_node.sync( sdt.FDT )
 
     # we have to turn the cpu mask into a name, and then apply it
     # to the rpu node for later
@@ -328,7 +340,6 @@ def xlnx_openamp_rpu( tgt_node, sdt, options ):
         core_conf = "sync"
     else:
         core_conf = "split"
-
     try:
         rpu_node['core_conf'].value = core_conf
         rpu_node.sync( sdt.FDT )
@@ -348,11 +359,6 @@ def xlnx_openamp_rpu( tgt_node, sdt, options ):
     #       utility in lopper and use it here and in the openamp domain
     #       processing.
     #
-    try:
-        memory_node = sdt.tree[ "/reserved-memory" ]
-    except:
-        memory_node = None
-
     if memory_node:
         if verbose:
             print( "[INFO]: memory node found, walking for memory regions" )
@@ -372,6 +378,10 @@ def xlnx_openamp_rpu( tgt_node, sdt, options ):
                 print( "[INFO]: setting memory-region to: %s" % phandle_list )
             try:
                 rpu_cpu_node = sdt.tree[ rpu_path + "/" + new_rpu_name ]
+                rpu_cpu_node["memory-region"].value = phandle_list
+                rpu_cpu_node.sync( sdt.FDT )
+
+
 
 
             except Exception as e:
@@ -492,12 +502,56 @@ def xlnx_openamp_rpu( tgt_node, sdt, options ):
                 # drop a trailing \0 if it was added above
                 mbox_names = mbox_names.rstrip('\0')
                 rpu_cpu_node["mboxes"].value = mboxes_prop
+                print(mbox_names)
                 rpu_cpu_node["mbox-names"].value = mbox_names
                 rpu_cpu_node.sync( sdt.FDT )
+    print("ret memory_node and exit gracefully")
+    return memory_node
 
-    ipis = parse_ipis_for_rpu(sdt, domain_node, rpu_cpu_node, options)
-    mem_carveouts = parse_memory_carevouts_for_rpu(sdt, domain_node, rpu_cpu_node, memory_node, options)
-    generate_openamp_file(ipis, mem_carveouts, options, platform)
+# tgt_node: is the openamp domain node number
+# sdt: is the system device tree
+# TODO: this routine needs to be factored and made smaller
+def xlnx_openamp_rpu( tgt_node, sdt, options ):
+    try:
+        verbose = options['verbose']
+    except:
+        verbose = 0
+
+    if verbose:
+        print( "[INFO]: cb: xlnx_openamp_rpu( %s, %s, %s )" % (tgt_node, sdt, verbose))
+
+    domain_node = sdt.tree[tgt_node]
+
+    root_node = sdt.tree["/"]
+    platform = SOC_TYPE.UNINITIALIZED
+    if 'versal' in str(root_node['compatible']):
+        platform = SOC_TYPE.VERSAL
+    elif 'zynqmp' in str(root_node['compatible']):
+        platform = SOC_TYPE.ZYNQMP
+    else:
+        print("invalid input system DT")
+        return False
+
+    # find the added rpu node
+    try:
+        rpu_node = sdt.tree[".*zynqmp-rpu" ]
+    except:
+        print( "[ERROR]: cannot find the target rpu node" )
+        rpu_node = None
+
+    try:
+        memory_node = sdt.tree[ "/reserved-memory" ]
+    except:
+        return False
+    ipis = parse_ipis_for_rpu(sdt, domain_node, options)
+    mem_carveouts = parse_memory_carevouts_for_rpu(sdt, domain_node, memory_node, options)
+    # last argument is for determining kernel case. if rpu_node exists, then is kernel case
+    [rsc_mem_pa,shared_mem_size] = generate_openamp_file(ipis, mem_carveouts, options, platform, (rpu_node != None) )
+    if rsc_mem_pa == -1:
+        print("[ERROR]: failed to generate openamp file")
+    if handle_rpmsg_kernelspace_case(tgt_node, sdt, options, domain_node, memory_node, rpu_node) != False:
+    else:
+        handle_rpmsg_userspace_case(tgt_node, sdt, options, domain_node, memory_node, rpu_node, rsc_mem_pa, shared_mem_size)
 
     return True
 
