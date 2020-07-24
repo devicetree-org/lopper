d06c58778376c55cb396ff25e9c1b083cacb4e7c
Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
2020-07-24 15:22:44 +0530

    assists: Add assists for generating cmake meta-data
    Xilinx Baremetal Driver examples or few libraries(ex:lwip) and
    it's examples supports a wide range of hardware features, All these
    hardware features may not be present in a given system-dt and
    application would like to know whether a particular feature is present
    or not.
    
    This patch adds support for the above use case by generating cmake
    meta-data, this meta-data is consumed by the Xilinx Baremetal applications.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

---
+++ assists/bmcmake_metadata_xlnx.py
@@ -0,0 +1,223 @@
+#/*
+# * Copyright (c) 2020 Xilinx Inc. All rights reserved.
+# *
+# * Author:
+# *       Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
+# *
+# * SPDX-License-Identifier: BSD-3-Clause
+# */
+
+import struct
+import sys
+import types
+import os
+import getopt
+import re
+from pathlib import Path
+from pathlib import PurePath
+from lopper import Lopper
+from lopper import LopperFmt
+import lopper
+from lopper_tree import *
+from re import *
+import yaml
+import glob
+from collections import OrderedDict
+
+sys.path.append(os.path.dirname(__file__))
+from baremetalconfig_xlnx import *
+
+def generate_drvcmake_metadata(sdt, node_list, yamlfile):
+    driver_compatlist = []
+
+    # Get the example_schema
+    with open(yamlfile, 'r') as stream:
+        schema = yaml.safe_load(stream)
+        driver_compatlist = compat_list(schema)
+        example_schema = schema['examples']
+       
+    driver_nodes = []
+    for compat in driver_compatlist:
+        for node in node_list:
+           compat_string = node['compatible'].value[0]
+           if compat in compat_string:
+               driver_nodes.append(node)
+
+    nodename_list = []
+    reg_list = []
+    example_dict = {}
+    depreg_dict = {}
+    for node in driver_nodes:
+        depreg_list = []
+        reg, size = scan_reg_size(node, node['reg'].value, 0)
+        nodename_list.append(node.name)
+        reg_list.append(hex(reg))
+        
+        validex_list = []
+        for example,prop in example_schema.items():
+            valid_ex = 0
+            match_list = []
+            for p in prop:
+                if isinstance(p, dict):
+                    for e,prop_val in p.items():
+                        try:
+                            val = node[e].value
+                            if '' in val:
+                                val = 1
+                            if e == "axistream-connected":
+                                reg = get_phandle_regprop(sdt, e, val)
+                                val = reg & 0xF
+                            if prop_val == "phandle":
+                                depreg_list.append(hex(get_phandle_regprop(sdt, e, val)))
+                        except KeyError:
+                            val = 0
+                        if prop_val == val:
+                            match_list.append(True)
+                        else:
+                            match_list.append(False)
+                else:
+                    try:
+                        valid_ex = node[prop[0]].value
+                    except KeyError:
+                        valid_ex = 0
+
+            if valid_ex or not False in match_list:
+                validex_list.append(example)
+
+        example_dict.update({node.name:validex_list})
+        depreg_dict.update({node.name:depreg_list})
+
+    drvname = yamlfile.rsplit('/', 1)[-1]
+    drvname = drvname.replace('.yaml', '')
+    cmake_file = drvname.capitalize() + str("Example.cmake")
+    with open(cmake_file, 'a') as fd:
+        fd.write("set(NUM_DRIVER_INSTANCES %s)\n" % to_cmakelist(nodename_list))
+        fd.write("set(REG_LIST %s)\n" % to_cmakelist(reg_list))
+        for index,name in enumerate(nodename_list):
+            fd.write("set(EXAMPLE_LIST%s %s)\n" % (index, to_cmakelist(example_dict[name])))
+            fd.write("set(DEPDRV_REG_LIST%s %s)\n" % (index, to_cmakelist(depreg_dict[name])))
+            fd.write("list(APPEND TOTAL_EXAMPLE_LIST EXAMPLE_LIST%s)\n" % index)
+            fd.write("list(APPEND TOTAL_DEPDRV_REG_LIST DEPDRV_REG_LIST%s)\n" % index)
+
+def getmatch_nodes(node_list, yamlfile):  
+    # Get the example_schema
+    with open(yamlfile, 'r') as stream:
+        schema = yaml.safe_load(stream)
+        driver_compatlist = compat_list(schema)
+       
+    driver_nodes = []
+    for compat in driver_compatlist:
+        for node in node_list:
+           compat_string = node['compatible'].value[0]
+           if compat in compat_string:
+               driver_nodes.append(node)
+
+    return driver_nodes
+
+def getxlnx_phytype(sdt, value):
+    child_node = sdt.FDT.node_offset_by_phandle(value[0])
+    name = sdt.FDT.get_name(child_node)
+    root_sub_nodes = sdt.tree['/'].subnodes()
+    child_node = [node for node in root_sub_nodes if re.search(name, node.name)]
+    phy_type = child_node[0]['xlnx,phy-type'].value[0]
+    return hex(phy_type)
+
+def lwip_topolgy(config):
+    topology_fd = open('xtopology_g.c', 'w')
+    tmp_str = "netif/xtopology.h"
+    tmp_str = '"{}"'.format(tmp_str)
+    topology_fd.write("\n#include %s\n" % tmp_str)
+    tmp_str = "xil_types.h"
+    tmp_str = '"{}"'.format(tmp_str)
+    topology_fd.write("#include %s\n\n" % tmp_str)
+    topology_fd.write("struct xtopology_t xtopology[] = {\n")
+    for index, data in enumerate(config):
+        if (index % 2) == 0:
+            topology_fd.write("\t{\n")
+        topology_fd.write("\t\t%s,\n" % data)
+        if (index % 2) != 0:
+            topology_fd.write("\n\t},\n")
+    topology_fd.write("\t{\n")
+    topology_fd.write("\t\tNULL\n")
+    topology_fd.write("\t}\n")
+    topology_fd.write("};")
+
+def generate_hwtocmake_medata(sdt, node_list, yamlfile):
+    meta_dict = {}
+    with open(yamlfile, 'r') as stream:
+        schema = yaml.safe_load(stream)
+        meta_dict = schema['HwConfig']
+
+    name = yamlfile.rsplit('/', 1)[-1]
+    name = name.replace('.yaml', '')
+    lwip = re.search("lwip211", name)
+    cmake_file = name.capitalize() + str("Example.cmake")
+    topology_data = []
+    with open(cmake_file, "a") as fd:
+        lwiptype_index = 0
+        for drv, prop_list in sorted(meta_dict.items(), key=lambda kv:(kv[0], kv[1])):
+            name = drv + str(".yaml")
+            drv_yamlpath = [y for x in os.walk(os.getcwd()) for y in glob.glob(os.path.join(x[0], name))]
+            nodes = getmatch_nodes(node_list, drv_yamlpath[0])
+            name_list = [node.name for node in nodes]
+            fd.write("set(%s_NUM_DRIVER_INSTANCES %s)\n" % (drv.upper(), to_cmakelist(name_list)))
+            for index,node in enumerate(nodes):
+                val_list = []
+                for prop in prop_list:
+                    if prop == "reg":
+                       reg,size = scan_reg_size(node, node[prop].value, 0)
+                       val = hex(reg)
+                       if lwip:
+                           topology_data.append(val)
+                           topology_data.append(lwiptype_index)
+                    elif prop == "interrupts":
+                       val = get_interrupt_prop(sdt.FDT, node, node[prop].value)
+                       val = val[0]
+                    elif prop == "axistream-connected":
+                       val = hex(get_phandle_regprop(sdt, prop, node[prop].value))
+                    elif prop == "phy-handle":
+                       try:
+                           val = getxlnx_phytype(sdt, node[prop].value)
+                       except KeyError:
+                           val = hex(0)
+                    else:
+                        val = hex(node[prop].value[0])
+                    val_list.append(val)
+                fd.write("set(%s%s_PROP_LIST %s)\n" % (drv.upper(), index, to_cmakelist(val_list)))
+                fd.write("list(APPEND TOTAL_%s_PROP_LIST %s%s_PROP_LIST)\n" % (drv.upper(), drv.upper(), index))
+            lwiptype_index += 1
+    if topology_data:
+        lwip_topolgy(topology_data)
+
+def is_compat( node, compat_string_to_test ):
+    if re.search( "module,bmcmake_metadata_xlnx", compat_string_to_test):
+        return xlnx_generate_cmake_metadata
+    return ""
+
+def to_cmakelist(pylist):
+    cmake_list = ';'.join(pylist)
+    cmake_list = '"{}"'.format(cmake_list)
+
+    return cmake_list
+
+def xlnx_generate_cmake_metadata(tgt_node, sdt, options):
+    root_node = sdt.tree[tgt_node]
+    root_sub_nodes = root_node.subnodes()
+
+    node_list = []
+    # Traverse the tree and find the nodes having status=ok property
+    for node in root_sub_nodes:
+        try:
+            status = node["status"].value
+            if "okay" in status:
+                node_list.append(node)
+        except:
+           pass
+
+    yamlfile = options['args'][0]
+    command = options['args'][1]
+    if command == "drvcmake_metadata":
+        generate_drvcmake_metadata(sdt, node_list, yamlfile)
+    elif command == "hwcmake_metadata":
+        generate_hwtocmake_medata(sdt, node_list, yamlfile)
+    return True
