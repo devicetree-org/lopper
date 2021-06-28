fa430a1c0a1eca5b4d239f2167edb1761d113823
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-06-28 15:13:11 -0700

    [wip] Add setuptools packaging boilerplate
    Add setup.py, which is an old-school style setuptools packaging
    script. I'm suggesting this style instead of the newer setup.cfg /
    pyproject.toml formats that are discussed in PEP 517 for two main
    reasons:
    
    1. PEP 517 was only provisionally accepted; the maintainers "reserve
       the right to make further changes to the specification based on our
       real world experience with those initial implementations" [1]
    
    2. PEP 517 based tooling does not support editable installs at this
       time, so you can't run "pip3 install -e ." from the root lopper
       directory and get a version of lopper that just runs live based on
       whatever is in git. This is a killer feature that makes development
       a lot easier, and is enough in its own right for me to stay on the
       old way of doing things, at least in Python packages I maintain.
    
    Add MANIFEST.in, which is required for getting the assist scripts.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>
    
    more manfiest

---
+++ setup.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2021 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import os
+from pathlib import Path
+
+import setuptools
+
+here = Path(__file__).parent
+
+with open(here / 'README', 'r') as f:
+    long_description = f.read()
+
+with open(here / 'lopper' / 'VERSION', 'r') as f:
+    # This is option 3 in:
+    # https://packaging.python.org/guides/single-sourcing-package-version/
+    version = f.read().strip()
+
+setuptools.setup(
+    name='lopper',
+    version=version,
+    author='Bruce Ashfield',
+    author_email='bruce.ashfield@gmail.com',
+    description='A devicetree pruner',
+    license='BSD',
+    long_description=long_description,
+    long_description_content_type='text/plain',
+    url='https://github.com/devicetree-org/lopper',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+    ],
+    packages=setuptools.find_packages(include=('lopper',)),
+    python_requires='>=3.5',
+    include_package_data=True,
+    install_requires=[],        # TODO
+    entry_points={'console_scripts': ('lopper = lopper.__main__:main',)},
+)
