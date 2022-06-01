791f0f55dd7d5c949c2800fca5e40f1f222a6338
Bruce Ashfield <bruce.ashfield@xilinx.com>
2022-06-01 15:44:40 -0400

    lopper.py: allow execution from any directory
    The lopper.py convenience function can't find our module(s) if it
    is run from another directory. We set the PTHONPATH before running
    to allow them to be found.
    
    Signed-off-by: Bruce Ashfield <bruce.ashfield@xilinx.com>

--- lopper.py
+++ lopper.py
@@ -2,5 +2,12 @@
 
 import sys
 import subprocess
+import os
+from os.path import dirname, abspath, join
 
-subprocess.run([sys.executable, '-m', 'lopper'] + sys.argv[1:])
+THIS_DIR = dirname(__file__)
+
+my_env = os.environ.copy()
+my_env["PYTHONPATH"] = THIS_DIR
+
+subprocess.run([sys.executable, '-m', 'lopper'] + sys.argv[1:], env=my_env )
