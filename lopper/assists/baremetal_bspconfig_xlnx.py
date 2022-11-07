da29be7393b1b3237c3a566b42a5dd5f81cf391c
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2022-11-07 11:24:22 +0530

    lopper: assists: baremetal_bspconfig_xlnx: Clean up the code
    This commit cleans up the assist code.
    
    Signed-off-by: Onkar Harsh <onkar.harsh@amd.com>
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>

--- lopper/assists/baremetal_bspconfig_xlnx.py
+++ lopper/assists/baremetal_bspconfig_xlnx.py
@@ -7,34 +7,18 @@
 # * SPDX-License-Identifier: BSD-3-Clause
 # */
 
-import struct
-import sys
-import types
-import unittest
-import os
-import getopt
-import re
-import subprocess
-import shutil
-from pathlib import Path
-from pathlib import PurePath
-from io import StringIO
-import contextlib
-import importlib
-from lopper import Lopper
-from lopper import LopperFmt
-import lopper
-from lopper.tree import *
-from re import *
 import yaml
+import sys
+import os
+import glob
 
 sys.path.append(os.path.dirname(__file__))
-from baremetalconfig_xlnx import *
-from baremetallinker_xlnx import *
+from baremetalconfig_xlnx import compat_list, get_cpu_node
+from baremetallinker_xlnx import get_memranges
 from bmcmake_metadata_xlnx import to_cmakelist
 
 def is_compat( node, compat_string_to_test ):
-    if re.search( "module,baremetal_bspconfig_xlnx", compat_string_to_test):
+    if "module,baremetal_bspconfig_xlnx" in compat_string_to_test:
         return xlnx_generate_bm_bspconfig
     return ""
 
@@ -49,63 +33,60 @@ def xlnx_generate_bm_bspconfig(tgt_node, sdt, options):
     if not mem_ranges:
         return
     # Generate Memconfig cmake meta-data file.
-    with open('MemConfig.cmake', 'w') as fd:
+    memconfig_path = os.path.join(sdt.outdir,'MemConfig.cmake')
+    with open(memconfig_path, 'w') as fd:
         mem_name_list = []
         mem_size_list = []
         for key, value in sorted(mem_ranges.items(), key=lambda e: e[1][1], reverse=True):
             start,size = value[0], value[1]
-            name = "XPAR_{}_BASEADDRESS".format(key.upper())
+            name = f"XPAR_{key.upper()}_BASEADDRESS"
             mem_name_list.append(name)
-            name = "XPAR_{}_HIGHADDRESS".format(key.upper())
+            name = f"XPAR_{key.upper()}_HIGHADDRESS"
             mem_name_list.append(name)
             mem_size_list.append(hex(start))
             mem_size_list.append(hex(start + size))
-        fd.write("set(MEM_DEF_NAMES %s)\n" % to_cmakelist(mem_name_list))
-        fd.write("set(MEM_RANGES %s)\n" % to_cmakelist(mem_size_list))
+        fd.write(f"set(MEM_DEF_NAMES {to_cmakelist(mem_name_list)})\n")
+        fd.write(f"set(MEM_RANGES {to_cmakelist(mem_size_list)})\n")
 
     machine = options['args'][0]
     # Get the cpu node for a given Processor
     match_cpunode = get_cpu_node(sdt, options)
 
-    tmpdir = os.getcwd()
-    os.chdir(options['args'][1])
-    os.chdir("../data")
-    cwd = os.getcwd()
-    files = os.listdir(cwd)
-    # Generate CPU specific config struct file.
-    for name in files:
-        os.chdir(cwd)
-        if os.path.isdir(name):
-            os.chdir(name)
-            yamlfile = name + str(".yaml")
-            with open(yamlfile) as stream:
-                schema = yaml.safe_load(stream)
-                compatlist = compat_list(schema)
-                prop_list = schema['required']
-                match = [compat for compat in compatlist if compat in match_cpunode['compatible'].value]
-                if match:
-                   config_struct = schema['config'][0] 
-                   outfile = tmpdir + str("/") + str("x") + name.lower() + str("_g.c")
-                   with open(outfile, 'w') as fd:
-                       fd.write('#include "x%s.h"\n' % name.lower())
-                       fd.write('\n%s %s __attribute__ ((section (".drvcfg_sec"))) = {\n' % (config_struct, config_struct + str("Table[]")))
-                       for index,prop in enumerate(prop_list):
-                           if index == 0:
-                               fd.write("\t{")
-                           try:
-                               for i in range(0, len(match_cpunode[prop].value)):
-                                   fd.write("\n\t\t%s" % hex(match_cpunode[prop].value[i]))
-                                   if i != (len(match_cpunode[prop].value) - 1):
-                                       fd.write(",")
-                           except:
-                               fd.write("\n\t\t 0")
-                           if prop == prop_list[-1]:
-                               fd.write("  /* %s */" % prop) 
-                               fd.write("\n\t}")
-                           else:
-                               fd.write(",")
-                               fd.write("  /* %s */" % prop) 
-                       fd.write("\n};")
-    os.chdir(tmpdir)
+    srcdir = options['args'][1].rstrip(os.sep)
+    datadir = os.path.join(os.path.dirname(srcdir),'data')
+    yaml_paths = glob.glob(f"{datadir}/*/*.yaml")
     
+
+    # Generate CPU specific config struct file.
+    for yamlfile in yaml_paths:
+        name = os.path.basename(os.path.dirname(yamlfile))
+        with open(yamlfile) as stream:
+            schema = yaml.safe_load(stream)
+            compatlist = compat_list(schema)
+            prop_list = schema['required']
+            match = [compat for compat in compatlist if compat in match_cpunode['compatible'].value]
+            if match:
+               config_struct = schema['config'][0]
+               outfile = os.path.join(sdt.outdir, f"x{name.lower()}_g.c")
+               with open(outfile, 'w') as fd:
+                   fd.write(f'#include "x{name.lower()}.h"\n')
+                   fd.write(f'\n{config_struct} {config_struct}Table[] __attribute__ ((section (".drvcfg_sec"))) = {{\n')
+                   for index,prop in enumerate(prop_list):
+                       if index == 0:
+                           fd.write("\t{")
+                       try:
+                           for i in range(0, len(match_cpunode[prop].value)):
+                               fd.write(f"\n\t\t{hex(match_cpunode[prop].value[i])}")
+                               if i != (len(match_cpunode[prop].value) - 1):
+                                   fd.write(",")
+                       except:
+                           fd.write("\n\t\t 0")
+                       if prop == prop_list[-1]:
+                           fd.write(f"  /* {prop} */")
+                           fd.write("\n\t}")
+                       else:
+                           fd.write(",")
+                           fd.write(f"  /* {prop} */") 
+                   fd.write("\n};")
+
     return True
