8ff2ef3b91110854e59d4813a683bc38ab80920f
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2024-01-10 12:48:22 +0530

    lopper: assists: gen_domain_dts: Update symbol node gic entries as per latest change
    commit 9883a0be34a7("lopper: lops: Rename the interrupt-controller node
    to inline with existing linux device-tree entries") renamed the
    interrupt-controller node need to update the symbol node entries for the
    same otherwise while applying overlay fixup will fail, commit fixes the
    same.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>

--- lopper/assists/gen_domain_dts.py
+++ lopper/assists/gen_domain_dts.py
@@ -197,6 +197,10 @@ def xlnx_generate_domain_dts(tgt_node, sdt, options):
     for prop in prop_list:
         if prop not in match_label_list:
             sdt.tree['/__symbols__'].delete(prop)
+        if prop == "gic_a53" or prop == "gic_a72" and linux_dt:
+            val = sdt.tree['/__symbols__'].propval(prop, list)[0]
+            val = val.replace("apu-bus", "axi")
+            sdt.tree['/__symbols__'].propval(prop, list)[0] = val
 
     # Add new property which will be consumed by other assists
     if not linux_dt:
