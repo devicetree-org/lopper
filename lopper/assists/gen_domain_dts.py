dd5d9058109399c1579096441334c4bdb1437d8a
Kunal Rane <kunal.rane@amd.com>
2024-10-01 22:45:02 +0530

    assists: gen_domain_dts: Ignore subcore nodes for mipi pipeline
    MIPI pipeline generates many sub-core nodes which are unncessary
    for linux solution. This can reduce final dtb size significantly.
    
    Signed-off-by: Kunal Rane <kunal.rane@amd.com>

--- lopper/assists/gen_domain_dts.py
+++ lopper/assists/gen_domain_dts.py
@@ -230,7 +230,8 @@ def xlnx_generate_domain_dts(tgt_node, sdt, options):
                             'psv_ocm', 'psv_pmc_aes', 'psv_pmc_bbram_ctrl', 'psv_pmc_cfi_cframe', 'psv_pmc_cfu_apb',
                             'psv_pmc_efuse_cache', 'psv_pmc_efuse_ctrl', 'psv_pmc_global', 'psv_pmc_ppu1_mdm',
                             'psv_pmc_ram_npi', 'psv_pmc_rsa', 'psv_pmc_sha', 'psv_pmc_slave_boot', 'psv_scntrs',
-                            'psv_pmc_slave_boot_stream', 'psv_pmc_trng', 'psv_psm_global_reg', 'psv_rpu', 'psv_scntr', 'v_tc']
+                            'psv_pmc_slave_boot_stream', 'psv_pmc_trng', 'psv_psm_global_reg', 'psv_rpu', 'psv_scntr', 'v_tc',
+                            'mipi_dphy', 'mipi_csi2_rx_ctrl', 'mipi_dsi_tx_ctrl', 'v_hscaler', 'v_vscaler', 'v_csc', 'v_hdmi_tx']
 
     if linux_dt:
         binding_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "yaml_bindings")
