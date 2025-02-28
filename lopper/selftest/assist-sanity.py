beff111c1b9fe03d6cb4e43ff60cf10816504a81
Bruce Ashfield <bruce.ashfield@amd.com>
2025-02-28 13:35:22 -0500

    lopper/sanity: add dts overlay sanity test
    Add a test to the selftest assist to ensure that we can
    set the type of "dts_overlay" and write a file that is
    of the correct overlay/plugin format.
    
    We also test that we can remove a node fro the original
    tree and write it out.
    
    Signed-off-by: Bruce Ashfield <bruce.ashfield@amd.com>

--- lopper/selftest/assist-sanity.py
+++ lopper/selftest/assist-sanity.py
@@ -21,12 +21,15 @@ import importlib
 from lopper import Lopper
 from lopper import LopperFmt
 from lopper.tree import LopperAction
+from lopper.tree import LopperTree, LopperNode
+from __init__ import LopperSDT
 import lopper
 import lopper_lib
 from itertools import chain
 from lopper_lib import chunks
 import copy
 from collections import OrderedDict
+import filecmp
 
 def is_compat( node, compat_string_to_test ):
     if re.search( "assist,domain-v1", compat_string_to_test):
@@ -166,5 +169,45 @@ def assist_reference( tgt_node, sdt, options ):
         print( "ooops: %s" % e )
 
     sdt.tree.print()
+
+    # pull the amba node out of the main tree, make it into a dtsi overlay
+    new_tree = LopperTree()
+    new_tree._type = "dts_overlay"
+    amba_node = sdt.tree["/amba"]
+
+    new_amba_node = amba_node()
+
+    # change/clear the label as a stress test
+    new_amba_node.name = "&foobar"
+    new_amba_node.label = ""
+
+    new_tree + new_amba_node
+
+    fpga_node = LopperNode( name="&fpga" )
+    fpga_node["external-fpga-config"] = [""]
+    new_tree + fpga_node
+
+    LopperSDT(None).write( new_tree, "/tmp/amba_overlay.dts", True, True )
+    LopperSDT(None).write( new_tree, "/tmp/amba_overlay.dtsi", True, True )
+
+    # compare the two files to ensure they are identical, the file extension
+    # is not a deciding factor, it is the type of the tree that makes the
+    # difference
+    if not filecmp.cmp( "/tmp/amba_overlay.dts", "/tmp/amba_overlay.dtsi" ):
+        print( "ERROR: /tmp/amba_overlay.dts and /tmp/amba_overlay.dtsi have differences" )
+        os._exit(1)
+
+    # remove the amba node from the original SDT
+    sdt.tree - amba_node
+
+    LopperSDT(None).write( sdt.tree, "/tmp/sdt_no_amba.dts", True, True )
+    count = 0
+    with open("/tmp/sdt_no_amba.dts") as fp:
+        for line in fp:
+            if re.search( r"amba_foo: amba", line ):
+                count += 1
+    if count != 0:
+        print( "ERROR: amba node was not remmoved from the SDT" )
+        os._exit(1)
 
     return 1
