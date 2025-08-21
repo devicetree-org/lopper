e7983094fa9933de427084750184c8a9cad0d849
Onkar Harsh <onkar.harsh@amd.com>
2025-08-21 23:49:04 +0530

    docs: Add docs directory for documenting AMD specific lops and assists usages
    Add docs directory for documenting AMD specific lops and assists usages. Add
    linux related usages to begin the documentation process.
    
    Signed-off-by: Onkar Harsh <onkar.harsh@amd.com>

---
+++ docs/amd/linux/source/conf.py
@@ -0,0 +1,21 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Linux Device Tree Generation for AMD SOCs using Lopper and SDT'
+copyright = '2025, Advanced Micro Devices, Inc.'
+author = 'Onkar Harsh <onkar.harsh@amd.com>'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
---
+++ docs/amd/linux/source/index.rst
@@ -0,0 +1,16 @@
+.. Linux Device Tree Generation for AMD SOCs using Lopper and SDT documentation master file, created by
+   sphinx-quickstart on Fri Aug 22 10:47:49 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Linux Device Tree Generation for AMD SOCs using Lopper and SDT's documentation!
+===========================================================================================
+
+.. toctree::
+   :maxdepth: 3
+   :hidden:
+
+   chapter1-introduction.rst
+   chapter2-linux_device_tree_generation.rst
+   chapter3-overlay_generation.rst
+
